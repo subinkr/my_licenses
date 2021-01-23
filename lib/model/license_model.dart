920e7d5ad30c0b3906daecf71b0f8c55ebd4da0c
subinkr <github@subin.kr>
2021-01-23 21:46:21 +0900

    Get CSV from Online

--- lib/model/license_model.dart
+++ lib/model/license_model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:csv/csv.dart';
+import 'package:http/http.dart' as http;
 
 class LicenseModel {
   List<List<dynamic>> data = [];
@@ -10,8 +11,15 @@ class LicenseModel {
   final licenseCode = 4; // 종목 코드
   final licenseName = 5; // 종목 명
 
-  loadLicenseList() async {
-    final myData = await rootBundle.loadString("license_list_20200625_model.csv");
-    return CsvToListConverter().convert(myData);
+  Future loadLicenseList() async {
+    final url = "https://raw.githubusercontent.com/subinkr/my_licenses/master/20200625.csv";
+    try {
+      var csvRead = await http.read(url);
+      print(csvRead.runtimeType);
+      return CsvToListConverter().convert(csvRead);
+      print(csvRead);
+    } catch(e) {
+      print('download error $e');
+    }
   }
 }
